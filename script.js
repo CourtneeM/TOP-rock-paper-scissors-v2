556486e2df6fd6e6381b991dd5923c8d2ed782b7
Courtnee McCollum <courtneemeaghan@gmail.com>
2022-03-21 16:13:55 -0500

    Add gameplay loop and prompt for player choice

--- script.js
+++ script.js
@@ -11,15 +11,51 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  // console.log(`Player: ${playerSelection} |`, `Computer: ${computerSelection}`);
-
   if (winningCombos[playerSelection].win === computerSelection) {
-    return 'The player wins!';
+    return { winner: 'player', winningMove: playerSelection, losingMove: computerSelection};
   } else if (winningCombos[computerSelection].win === playerSelection) {
-    return 'The computer wins!';
+    return { winner: 'computer', winningMove: computerSelection, losingMove: playerSelection};
   } else {
-    return "It's a tie!";
+    return 'tie';
   }
 }
 
-console.log(playRound('rock', computerPlay()));
+function game() {
+  const scores = { player: 0, computer: 0 };
+  const matchHistory = [];
+  const moves = ['rock', 'paper', 'scissors'];
+  
+  let roundsToPlay = 5;
+  let playerSelection = prompt('Which move would you like to make? rock, paper, or scissors?').toLowerCase();
+
+  while (!moves.includes(playerSelection)) {
+    alert(`${playerSelection} is not a valid choice.`);
+    playerSelection = prompt('Which move would you like to make? rock, paper, or scissors?');
+  }
+
+  while (roundsToPlay > 0) {
+    let { winner, winningMove, losingMove } = playRound(playerSelection, computerPlay());
+     
+    if (winner === 'player' || winner === 'computer') {
+      winner === 'player' ? scores.player++ : scores.computer++; 
+      matchHistory.push(`The ${winner} wins! ${winningMove} beats ${losingMove}`);
+    } else {
+      matchHistory.push("It's a tie!");
+    }
+    
+    roundsToPlay--;
+  }
+
+  console.log(`Round breakdown:\n`);
+  matchHistory.forEach(round => console.log(round));
+
+  if (scores.player > scores.computer) {
+    return `Player beats computer ${scores.player} to ${scores.computer}!`;
+  } else if (scores.computer > scores.player) {
+    return `Computer beats player ${scores.computer} to ${scores.player}!`;
+  } else {
+    return `It's a tie! ${scores.player} to ${scores.computer}`;
+  }
+}
+
+console.log(game());
